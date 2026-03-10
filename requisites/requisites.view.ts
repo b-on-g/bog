1b45c942d265821b8b5f1635b0b47908126022b0
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-10 22:31:57 +0300

    общие реквизиты мои, для встраивания на сайт для подключения платежки
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

---
+++ requisites/requisites.view.ts
@@ -0,0 +1,9 @@
+namespace $.$$ {
+
+	export class $bog_project_tree_appname_requisites extends $.$bog_project_tree_appname_requisites {
+
+		// Базовый класс, дополнительная логика не требуется
+
+	}
+
+}
