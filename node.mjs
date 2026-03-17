a2a992e0b8c7d4fc7d5935c1cf90a25581f042c3
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-17 07:54:47 +0000

    Deploying to gh-pages from @ b-on-g/bog@dac16cef87ed82315a862dccd62b72746f5e3e73 🚀

--- node.mjs
+++ node.mjs
@@ -8484,6 +8484,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
--- node.test.js
+++ node.test.js
@@ -8475,6 +8475,8 @@ var $;
                 }
                 catch (error) {
                     $mol_fail_log(error);
+                    if (error instanceof Error)
+                        return '💥' + error.message;
                     return '';
                 }
             }
