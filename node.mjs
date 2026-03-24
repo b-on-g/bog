2717b7e7f0d9af5092cd27f4671400681f2338f7
Dev-cmyser <Dev-cmyser@users.noreply.github.com>
2026-03-24 07:54:54 +0000

    Deploying to gh-pages from @ b-on-g/bog@ceb98f940d925cfe9de476b1d8292e0914dfd2b4 🚀

--- node.mjs
+++ node.mjs
@@ -10689,6 +10689,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    let x = /x/[Symbol.matchAll];
     class $mol_regexp extends RegExp {
         groups;
         constructor(source, flags = 'gsu', groups = []) {
