d68b6ec7a4049ea6904d78f7b87ef046f0f2915c
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-03-26 08:44:17 +0300

    sd
    Signed-off-by: novgorodcev.k <novgorodcev.k@rwb.ru>

--- feedback/form/form.view.ts
+++ feedback/form/form.view.ts
@@ -1,6 +1,6 @@
 namespace $.$$ {
 	const LAND_ID = 'iexkmPYx_9RQxWRZF'
-	const OWNER_LORDS = ['30rPfnwR_eBFtt8H6', 'G4l4UZr3_ibnzXhgQ']
+	const OWNER_LORDS = ['30rPfnwR_eBFtt8H6', 'G4l4UZr3_ibnzXhgQ', 'BJ7CAnEP_XYEV6Z67']
 	const Entries_dict = $giper_baza_dict_to($bog_feedback_entry)
 
 	export class $bog_feedback_form extends $.$bog_feedback_form {
