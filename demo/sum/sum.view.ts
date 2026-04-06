8583f563eef293f849629750065194c7ad51ad77
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-04-06 17:16:42 +0300

    df

--- demo/sum/sum.view.ts
+++
@@ -1,8 +0,0 @@
-namespace $.$$ {
-	export class $bog_demo_sum extends $.$bog_demo_sum {
-		@ $mol_mem
-		result() {
-			return `${ this.a().toFixed(2) } + ${ this.b() } = ${ this.a() + this.b() }`
-		}
-	}
-}
